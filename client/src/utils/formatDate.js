b2c26615197b60f7ace80f06377066b9b054f41c
Joel Buhrman <joel.buhrman@live.se>
2019-03-11 15:02:18 +0100

    Create formatDate and add jest to project

---
+++ client/src/utils/formatDate.js
@@ -0,0 +1,4 @@
+const formatDate = date => date.replace('T', ' ').slice(0, -3)
+module.exports = {
+  formatDate
+}
---
+++ client/src/utils/formatDate.spec.js
@@ -0,0 +1,9 @@
+const { formatDate } = require('./formatDate')
+
+describe('formatDate', () => {
+  const input = '2019-03-16T16:20:00'
+  const expectedOutput = '2019-03-16 16:20'
+  it('should format the date correctly', () => {
+    expect(formatDate(input)).toBe(expectedOutput)
+  })
+})
