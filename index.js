7d41f855138e302db94bd0673874ee2433d6ecd8
Joel Buhrman <joel.buhrman@live.se>
2019-03-11 11:07:43 +0100

    Create a server api making the actual requests

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const path = require('path')
+const https = require('https')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+app.get('/getGames/:gameType', (req, res) => {
+  const { gameType } = req.params
+  https
+    .get(
+      'https://www.atg.se/services/racinginfo/v1/api/products/' + gameType,
+      resp => {
+        let data = ''
+        resp.on('data', chunk => {
+          data += chunk
+        })
+        resp.on('end', () => {
+          res.send(JSON.parse(data))
+        })
+      }
+    )
+    .on('error', err => {
+      console.log('Error: ' + err.message)
+    })
+})
+
+app.get('getGameData/:gameId', (req, res) => {
+  const { gameId } = req.params
+  https
+    .get(
+      'https://www.atg.se/services/racinginfo/v1/api/games/' + gameId,
+      resp => {
+        let data = ''
+        resp.on('data', chunk => {
+          data += chunk
+        })
+        resp.on('end', () => {
+          res.send(JSON.parse(data))
+        })
+      }
+    )
+    .on('error', err => {
+      console.log('Error: ' + err.message)
+    })
+})
+
+const port = process.env.PORT || 5000
+app.listen(port)
