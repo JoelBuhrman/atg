b54f48f5b150c569c6269ba81cb7911ae36bcf50
Joel Buhrman <joel.buhrman@live.se>
2019-03-11 11:18:48 +0100

    Add fetchGames method to input

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import Input from './components'
+import { getGames } from './api'
 
 const Container = styled.div`
   width: 100%;
 `
 
-const App = () => (
-  <Container>
-    <Input />
-  </Container>
-)
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      games: []
+    }
+  }
+
+  fetchGames = async gameType => this.setState({ games: await getGames('V75') })
+
+  render() {
+    return (
+      <Container>
+        <Input fetchGames={this.fetchGames} />
+      </Container>
+    )
+  }
+}
 
 export default App
