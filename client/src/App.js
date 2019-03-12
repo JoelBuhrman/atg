da2cb604fd2d291f2da5bab933302b8928ca4bee
Joel Buhrman <joel.buhrman@live.se>
2019-03-12 10:50:04 +0100

    Add loader

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,12 @@ const Error = styled.div`
   margin-top: 20px;
 `
 
+const Loader = styled.img`
+  width: 50px;
+  margin-left: calc(50% - 25px);
+  margin-top: 20px;
+`
+
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -26,13 +32,19 @@ class App extends React.Component {
   }
 
   fetchGames = async e => {
+    this.setState({ loading: true, games: null, error: null })
     e.preventDefault()
     const inputValue = document.getElementById('input').value
     try {
       const value = await getGames(inputValue)
-      this.setState({ games: value, type: inputValue, error: null })
+      this.setState({
+        games: value,
+        type: inputValue,
+        error: null,
+        loading: false
+      })
     } catch (Error) {
-      this.setState({ error: 'error', games: null, type: null })
+      this.setState({ error: 'error', games: null, type: null, loading: false })
       setTimeout(() => this.setState({ error: null }), 4000)
     }
   }
@@ -50,6 +62,9 @@ class App extends React.Component {
         )}
         {this.state.error && (
           <Error>Please search for one of V75, V65, V64, V4</Error>
+        )}
+        {this.state.loading && (
+          <Loader src='https://cwsmgmt.corsair.com/_ui/responsive/common/img/pdp/zoom_loader.gif' />
         )}
       </Container>
     )
